bcb5139a8c83317363385e181335b95451524e3b
Maipady, Anil <Anil.Maipady@ca.com>
2017-09-01 19:01:39 -0700

    Adding collaborative filter example

--- src/main/scala/org/ca/learn/CollaborativeFilter.scala
+++ src/main/scala/org/ca/learn/CollaborativeFilter.scala
@@ -14,7 +14,8 @@ object CollaborativeFilter {
       .appName("CollaborativeFilter")
       .getOrCreate()
     val encoder = Encoders.product[Rating]
-    val ratings = spark.read.option("header", "true").option("inferschema", "true").csv("./src/main/scala/resource/ratings.csv").as[Rating](encoder).cache()
+    //https://sparkcookbook.s3.amazonaws.com/moviedata/ratings.csv
+    val ratings = spark.read.option("header", "true").option("inferschema", "true").csv("./src/main/scala/resource/ratings-do-not-commit.csv").as[Rating](encoder).cache()
     val Array(training, test) = ratings.randomSplit(Array(0.7, 0.3))
     val als = new ALS().setMaxIter(30).setRegParam(.065).setUserCol("userId").setItemCol("movieId").setRatingCol("rating")
     val model = als.fit(training)
@@ -27,5 +28,30 @@ object CollaborativeFilter {
       .setPredictionCol("prediction")
     val rmse = evaluator.evaluate(predictions)
     println(s"Root-mean-square error = $rmse")
+
+
+    val myrecs = spark.createDataFrame(Seq(
+      (138494,1721,5,1489789319),
+      (138494,10,3,1489789319),
+      (138494,1,1,1489789319),
+      (138494,225,4,1489789319),
+      (138494,344,4,1489789319),
+      (138494,480,5,1489789319),
+      (138494,589,5,1489789319),
+      (138494,780,4,1489789319),
+      (138494,1049,4,1489789319)
+    )).toDF("userId","movieId","rating","timestamp").as[Rating](encoder)
+
+    val trainingWithMyRecs = training.union(myrecs)
+
+    val model2 = als.fit(trainingWithMyRecs)
+
+    val terminator = spark.createDataFrame(Seq((138494,1240) )).toDF("userId","movieId")
+
+    val p = model2.transform(terminator)
+
+    p.show()
+
+
   }
 }
---
+++ src/main/scala/org/ca/learn/CollaborativeFilterImplicitFeedback.scala
@@ -0,0 +1,41 @@
+package org.ca.learn
+
+import org.apache.spark.ml.recommendation.ALS
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.sql.{Dataset, Encoders, SparkSession}
+
+import org.apache.spark.ml.recommendation.ALS
+import org.apache.spark.sql.functions._
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+
+object CollaborativeFilterImplicitFeedback {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder
+      .appName("CollaborativeFilter")
+      .getOrCreate()
+    val encoder = Encoders.product[Rating]
+    val als = new ALS().setImplicitPrefs(true).setUserCol("userId").setItemCol("songId").setRatingCol("plays")
+    val songs = spark.read.option("delimiter"," ").option("inferschema","true").csv("./src/main/scala/resource/kaggle_songs.txt").toDF("song","songId")
+    songs.show()
+    songs.createOrReplaceTempView("songs")
+    val users = spark.read.textFile("./src/main/scala/resource/kaggle_users.txt").toDF("user").withColumn("userId",monotonically_increasing_id)
+    users.show()
+    val u =  users.withColumn("userId",users.col("userId").cast("integer"))
+    u.createOrReplaceTempView("users")
+    val triplets1 = spark.read.option("delimiter",",").option("inferschema","true").csv("./src/main/scala/resource/kaggle_visible_evaluation_triplets.txt")
+    triplets1.show()
+    val triplets  = triplets1.toDF("user","song","plays")
+    val t = triplets.withColumn("plays",triplets.col("plays").cast("double"))
+    t.createOrReplaceTempView("plays")
+    val plays = spark.sql("select userId,songId,plays from plays p join users u on p.user = u.user join songs s on p.song = s.song")
+    val Array(training, test) = plays.randomSplit(Array(0.7, 0.3))
+    val model = als.fit(training)
+    val predictions = model.transform(test).na.drop
+    val evaluator = new RegressionEvaluator().setMetricName("mae").setLabelCol("plays").setPredictionCol("prediction")
+    val rmse = evaluator.evaluate(predictions)
+    println(s"Root-mean-square error = $rmse")
+
+  }
+}
